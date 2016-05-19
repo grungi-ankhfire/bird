1175bc9b631520121fd05ab5d03abf872b5a080b
Bastien Gorissen <bastien.gorissen@external.engie.com>
2016-05-19 13:09:14 +0200

    Work on signal analysis

--- bird/Assets/Scripts/BeatDetector.cs
+++ bird/Assets/Scripts/BeatDetector.cs
@@ -1,24 +1,98 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class BeatDetector : MonoBehaviour {
 
-    AudioSource audiosource;
+    AudioSource aud;
     float[] spectrum = new float[1024];
+    public float C = 1.3f; 
+    private bool[] beats;
+
+    private int delay = 1;
 
     void Start() {
-        audiosource = GetComponent<AudioSource>();
+        aud = GetComponent<AudioSource>();
+        int num_samples = aud.clip.samples * aud.clip.channels;
+        float[] samples = new float[num_samples];
+        int num_windows = (int) Mathf.Ceil(num_samples / aud.clip.channels / 1024.0f);
+        float[] energies = new float[num_windows];
+        beats = new bool[num_windows];
+        float energy_left = 0f;
+        float energy_right = 0f;
+        float sample_left;
+        float sample_right;
+        float energy_history;
+        float current_sample;
+
+        print(num_windows);
+        print(num_samples);
+
+        aud.clip.GetData(samples, 0 );
+        int number_of_beats = 0;
+
+        float min_variance = 10000000000f;
+        float max_variance = -10000000000f;
+
+        for (int w=0; w<num_windows; w++) {
+
+            int window_length = 1024;
+            if (w == num_windows-1) {
+                window_length = (num_samples/aud.clip.channels - (num_windows-1) * 1024);
+            }
+
+            for (int i=0; i<window_length; i++){
+                for (int c = 0; c < aud.clip.channels; c++) {
+                    current_sample = samples[i*aud.clip.channels + c + w*1024*aud.clip.channels];
+                    energies[w] +=  current_sample * current_sample;
+                }
+
+
+            }
+            //energies[w] = energy_left + energy_right;
+            if (w > 42) {
+                energy_history = 0f;
+                for (int j = w-43; j < w; j++) {
+                    energy_history += energies[j];
+                }
+                energy_history *= 1f/43f;
+
+                float variance = 0f;
+                for (int j = w-43; j < w; j++) {
+                    variance += (energies[j] - energy_history) * (energies[j] - energy_history);
+                }
+                variance *= 1f/43f;
+                if (variance < min_variance) {
+                    min_variance = variance;
+                }
+                if (variance > max_variance) {
+                    max_variance = variance;
+                }
+                C = (-0.0025714f*variance) +1.5142857f;
+
+                if (energy_history*C < energies[w]) {
+                    beats[w] = true;
+                    number_of_beats += 1;
+                } else {
+                    beats[w] = false;
+                }
+            }
+        }
+        print("number of beats : " + number_of_beats);
+        print("Min variance : " + min_variance);
+        print("Max variance : " + max_variance);
     }
 
-    void Update() {
-        audiosource.GetSpectrumData(spectrum, 1, FFTWindow.BlackmanHarris);
-        int i = 1;
-        while (i < spectrum.Length-1) {
-            //Debug.DrawLine(new Vector3(i - 1, spectrum[i] + 10, 0), new Vector3(i, spectrum[i + 1] + 10, 0), Color.red);
-            //Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-            //Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.yellow);
-            i++;
+    void FixedUpdate() {
+        //print(aud.timeSamples);
+        if (beats[(int) Mathf.Floor(aud.timeSamples / 1024.0f)]) {
+            transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        } else {
+            if (delay == 0) {
+                delay = 1;
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            } else {
+                delay -= 1;
+            }
         }
     }
 
