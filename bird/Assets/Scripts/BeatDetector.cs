de21f4b54102135cca5b890633496257befb3c06
Bastien Gorissen <bastien.gorissen@external.engie.com>
2016-06-21 17:57:35 +0200

    More progress

--- bird/Assets/Scripts/BeatDetector.cs
+++ bird/Assets/Scripts/BeatDetector.cs
@@ -2,109 +2,212 @@ using UnityEngine;
 using System.Collections;
 using SpeedTest;
 
+[RequireComponent(typeof (AudioSource))]
 public class BeatDetector : MonoBehaviour {
 
-    AudioSource aud;
-    private float[] spectrum;
-    public float C = 1.3f;
-    public int sample_window = 1024;
+    // For the precomputed analysis
+    public int maxWindowSize = 1024;
+    
+    private AudioSource aud;
+    private float C = 1.3f;
     private bool[] beats;
-
-    private int delay = 1;
-
     private FFT2 fft;
+    private int channels;
+    private int numSamples;
+    private int numWindows;
+    private float[] energies;
+    private float energyBuffer;
+    private int beatNumber;
+    private int windowSize;
+    private float variance;
+    private float minVariance;
+    private float maxVariance;
+    private float minEnergy;
+    private float maxEnergy;
+
+    // For in-game use
+    private int delay = 1;
+    private int previousSegment;
+    private int segment = 0;
+
+    private float[] rawSamples;
+    private float[] samplesFFT;
+    private float[] samplesFFTi;
+
+
+    private float[] trajectory;
+
+    // For road construction
+    private float[] elevations;
 
     void Start() {
-        aud = GetComponent<AudioSource>();
-        int num_samples = aud.clip.samples * aud.clip.channels;
-        float[] samples = new float[num_samples];
-        int num_windows = (int) Mathf.Ceil(num_samples / aud.clip.channels / 1024.0f);
-        float[] energies = new float[num_windows];
-        beats = new bool[num_windows];
-        float energy_history;
-        float current_sample;
 
+        // Get AudioClip informations
+        aud = GetComponent<AudioSource>();
+        channels = aud.clip.channels;
+        numSamples = aud.clip.samples;
+        rawSamples = new float[numSamples * channels];
+        aud.clip.GetData(rawSamples, 0);
+        numWindows = (int) Mathf.Ceil(numSamples / (float) maxWindowSize);
+
+        
+        // Allocate objects for spectrum analysis
+        energies = new float[numWindows];
+        beats = new bool[numWindows];
         fft = new FFT2();
         fft.init(10);
+        minVariance = 10000000000f;
+        minEnergy = 10000000000f;
+        maxVariance = -10000000000f;
+        maxEnergy = -10000000000f;
+        samplesFFT = new float[maxWindowSize];
+        samplesFFTi = new float[maxWindowSize];
+        beatNumber = 0;
+        elevations = new float[numWindows];
+        trajectory = new float[numWindows];
 
-        print(num_windows);
-        print(num_samples);
+        Debug.Log("Number of samples : " + numSamples);
+        Debug.Log("Number of windows : " + numWindows);
 
-        float[] samples_fft = new float[sample_window];
-        float[] samples_fft_i = new float[sample_window];
-        spectrum = new float[sample_window];
-        aud.clip.GetData(samples, 0);
-        int number_of_beats = 0;
+        for (int w=0; w<numWindows; w++) {
 
-        float min_variance = 10000000000f;
-        float max_variance = -10000000000f;
-
-        for (int w=0; w<num_windows; w++) {
-
-            int window_length = sample_window;
-            if (w == num_windows-1) {
-                window_length = (num_samples/aud.clip.channels - (num_windows-1) * sample_window);
+            windowSize = maxWindowSize;
+            if (w == numWindows-1) {
+                windowSize = numSamples - (numWindows - 1) * maxWindowSize;
             }
             
-            for (int c = 0; c < aud.clip.channels; c++) {
-                for (int s = 0; s < sample_window; s++) {
-                    if (window_length < sample_window && s >= window_length) {
-                            samples_fft[s] = 0f;
+            rawSamples = new float[windowSize * channels];
+            aud.clip.GetData(rawSamples, w*maxWindowSize);
+
+            for (int c = 0; c < channels; c++) {
+                for (int s = 0; s < maxWindowSize; s++) {
+                    if (windowSize < maxWindowSize && s >= windowSize) {
+                        samplesFFT[s] = 0f;
                     }
                     else {
-                        samples_fft[s] = samples[w*sample_window + s*aud.clip.channels + c];
+                        samplesFFT[s] = rawSamples[s * channels + c];
                     }
-                    samples_fft_i[s] = 0f;
-                
+                    elevations[w] += Mathf.Abs(samplesFFT[s]);
+                    samplesFFTi[s] = 0f;
                 }
+                samplesFFT = BlackmanHarris(samplesFFT);
+                fft.run(samplesFFT, samplesFFTi, false);
 
-                fft.run(samples_fft, samples_fft_i, false);
-
-                for (int i=0; i<window_length/2; i++){
-                    current_sample = samples_fft[i];
-                    energies[w] += (samples_fft[i] * samples_fft[i] + samples_fft_i[i] * samples_fft_i[i]);
-                }
+                for (int i = 0; i < maxWindowSize/16; i++){
+                    energies[w] += norm(samplesFFT[i], samplesFFTi[i], true);
+                } 
+                // energies[w] *= 2;
             }
+            elevations[w] = -(elevations[w] / (channels * maxWindowSize)) * 2f + 0.5f;
+            if (elevations[w] < 0) {
+                elevations[w] *= 2f;
+            }
+            if (w > 0) {
+                elevations[w] += elevations[w-1];
+            }
+            minEnergy = Mathf.Min(energies[w], minEnergy);
+            maxEnergy = Mathf.Max(energies[w], maxEnergy);
 
-            //energies[w] = energy_left + energy_right;
             if (w > 42) {
-                energy_history = 0f;
+                energyBuffer = 0f;
                 for (int j = w-43; j < w; j++) {
-                    energy_history += energies[j];
+                    energyBuffer += energies[j];
                 }
-                energy_history *= 1f/43f;
+                energyBuffer *= 1f / 43f;
 
-                float variance = 0f;
+                if (energies[w] > energyBuffer || energies[w] > 0.25*maxEnergy) {
+                    trajectory[w] = trajectory[w-1] - 1.0f;    
+                } else if (Mathf.Abs(energyBuffer - energies[w]) < 1.0f) {
+                    trajectory[w] = trajectory[w-1];
+                } else {
+                    trajectory[w] = trajectory[w-1] + 1.0f;
+                }
+                trajectory[w] = trajectory[w-1] + (energyBuffer-energies[w]) / 10f; //maxEnergy * 2f;
+
+                variance = 0f;
                 for (int j = w-43; j < w; j++) {
-                    variance += (energies[j] - energy_history) * (energies[j] - energy_history);
+                    variance += (energies[j] - energyBuffer) * (energies[j] - energyBuffer);
                 }
-                variance *= 1f/43f;
-                if (variance < min_variance) {
-                    min_variance = variance;
-                }
-                if (variance > max_variance) {
-                    max_variance = variance;
-                }
-                C = (-0.0025714f*variance) +1.5142857f;
+                variance /= 43f;
+                minVariance = Mathf.Min(variance, minVariance);
+                maxVariance = Mathf.Max(variance, maxVariance);
 
-                if (energy_history*C < energies[w]) {
+                C = (-0.0005f*variance) +2f;
+                //C = 1.4f;
+                if (C*energyBuffer < energies[w]) {
+                //if (10 < energies[w]) {
                     beats[w] = true;
-                    number_of_beats += 1;
+                    beatNumber += 1;
                 } else {
                     beats[w] = false;
                 }
             }
         }
-        print("number of beats : " + number_of_beats);
-        print("Min variance : " + min_variance);
-        print("Max variance : " + max_variance);
+
+        print("Minimum energy : " + minEnergy);
+        print("Maximum energy : " + maxEnergy);
+        print("Number of beats : " + beatNumber);
+        print("Minimum variance : " + minVariance);
+        print("Maximum variance : " + maxVariance);
+        rawSamples = new float[maxWindowSize*channels];
     }
 
     void FixedUpdate() {
-        //print(aud.timeSamples);
-        if (beats[(int) Mathf.Floor(aud.timeSamples / 1024.0f)]) {
+        previousSegment = segment;
+        segment = (int) Mathf.Floor(aud.timeSamples / maxWindowSize);
+
+        int energyStart = (int) Mathf.Max(0, segment-512);
+        int energyEnd = (int) Mathf.Min(numWindows, segment + 512);
+
+        for (int w = energyStart; w < energyEnd; w++) {
+            if (w == segment) {
+                Debug.DrawLine(new Vector3(0.01f*(w-energyStart + 520), 0, 0), new Vector3(0.01f*(w-energyStart + 520), energies[w]/maxEnergy, 0), Color.white);
+                Debug.DrawLine(new Vector3(0, elevations[w], w-energyStart), new Vector3(0, elevations[w+1], (w+1)-energyStart), Color.yellow);
+            } else {
+                Debug.DrawLine(new Vector3(0.01f*(w-energyStart + 520), 0, 0), new Vector3(0.01f*(w-energyStart + 520), energies[w]/maxEnergy, 0), Color.black);
+                Debug.DrawLine(new Vector3(0, elevations[w], w-energyStart), new Vector3(0, elevations[w+1], (w+1)-energyStart), Color.black);
+            }
+        }
+
+        if (segment != previousSegment) {
+            samplesFFT = new float[maxWindowSize];
+            samplesFFTi = new float[maxWindowSize];
+            aud.clip.GetData(rawSamples, segment*maxWindowSize);
+            for (int c = 1; c < channels; c++) {
+                for (int s = 0; s < maxWindowSize; s++) {
+                    samplesFFT[s] = rawSamples[s*channels + c];
+                    samplesFFTi[s] = 0f;
+                }
+                samplesFFT = BlackmanHarris(samplesFFT);
+                fft.run(samplesFFT, samplesFFTi, false);
+            }
+        }
+        float newNorm = 0f;
+        float oldNorm = 0f;
+        Color col = Color.red;
+        oldNorm += norm(samplesFFT[0], samplesFFTi[0], true);
+
+        float energy = oldNorm;
+        for (int i = 0; i < 511; i++) {
+            newNorm = 0f;
+            newNorm += norm(samplesFFT[i], samplesFFTi[i], true);
+            energy += newNorm;
+
+            Debug.DrawLine(new Vector3(0.01f*i, oldNorm, 0), new Vector3(0.01f*(i+1), newNorm, 0), col);
+            oldNorm = newNorm;
+        }
+
+        Debug.DrawLine(new Vector3(-1, 0, 0), new Vector3(-1, energy, 0), Color.green);
+
+        if (energy > 10) {
+            Debug.DrawLine(new Vector3(-1.2f, 0, 0), new Vector3(-1.1f, 1, 0), Color.black);
+        }
+        if (beats[segment]) {
+            Debug.DrawLine(new Vector3(-1.1f, 0, 0), new Vector3(-1.1f, 1, 0), Color.blue);
             transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
         } else {
+            Debug.DrawLine(new Vector3(-1.1f, 0, 0), new Vector3(-1.1f, 1, 0), Color.yellow);
+
             if (delay == 0) {
                 delay = 1;
                 transform.localScale = new Vector3(1f, 1f, 1f);
@@ -113,5 +216,22 @@ public class BeatDetector : MonoBehaviour {
             }
         }
     }
+
+    float[] BlackmanHarris(float[] signal) {
+        float N = maxWindowSize;
+        for (int n = 0; n < signal.Length; n++) {
+            signal[n] *= 0.35875f - (0.48829f * Mathf.Cos(1.0f * n/N)) + (0.14128f * Mathf.Cos(2.0f * n/N)) - (0.01168f * Mathf.Cos(3.0f * n/N));
+        }
+        return signal;
+    }
+
+    float norm(float real, float complex, bool squared=false) {
+        if (squared) {
+            return real*real + complex*complex;
+        } else {
+            return Mathf.Sqrt(real*real + complex*complex);
+        }
+    }
+
 
 }
